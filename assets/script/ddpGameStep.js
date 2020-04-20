cd0708cd28a083fd7e74a50281af0642d0081273
qdpoboy <280597526@qq.com>
2020-04-20 22:07:38 +0800

    适配

---
+++ assets/script/ddpGameStep.js
@@ -0,0 +1,31 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        step1: {
+            default: null,
+            type: cc.Sprite
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.step1.node.on(cc.Node.EventType.TOUCH_START, function() {
+            cc.director.loadScene('ddpGame');
+        }, this);
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
